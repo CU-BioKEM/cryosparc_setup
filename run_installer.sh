b3a800678c9e09cc9330c1de2d090fa640335938
CU-BioKEM <84194607+CU-BioKEM@users.noreply.github.com>
2023-02-15 13:07:12 -0700

    Update run_installer.sh

--- run_installer.sh
+++ run_installer.sh
@@ -1,4 +1,4 @@
-~/cryosparc/cryosparc_master/install.sh --license 9d16da3a-1179-11ec-9e71-03daca9071c5 \
-             --hostname trail-cryosparc \
-             --dbpath /pl/active/LugerLab-EM/software/cryosparc/cryosparc_database \
+~/cryosparc/cryosparc_master/install.sh --license $LICENSE_ID \
+             --hostname <host>\
+             --dbpath /pl/active/BioKEM/software/cryosparc/<lab>/cryosparc_database \
              --port 39000 
