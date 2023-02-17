14a5c6d918dd8b74f0b411c3a55effc33ac7e4a4
Shawn Laursen <shla9937@colorado.edu>
2023-02-17 10:49:34 -0700

    added blanca

---
+++ blanca/cluster_script.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#SBATCH --partition=blanca
+#SBATCH --qos=preemptable
+#SBATCH --account=blanca-biokem
+#SBATCH --nodes=1
+#SBATCH --ntasks={{ num_cpu }}
+#SBATCH --gres=gpu:{{ num_gpu }}
+#SBATCH --time=24:00:00
+#SBATCH --mem={{ (ram_gb*2)|int }}GB
+#SBATCH --job-name=cspark_{{ project_uid }}_{{ job_uid }}
+#SBATCH --output={{ job_log_path_abs }}
+#SBATCH --error={{ job_log_path_abs }}
+
+{{ run_cmd }}
