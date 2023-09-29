ab17978a12b589e16cfce94d983ea4534912f22a
Shawn Laursen <shla9937@colorado.edu>
2023-09-29 09:41:50 -0500

    Update run_first_user.sh

--- run_first_user.sh
+++ run_first_user.sh
@@ -3,3 +3,9 @@ cryosparcm createuser --email "shla9937@colorado.edu" \
                       --username "shla9937" \
                       --firstname "Shawn" \
                       --lastname "Laursen"
+
+cryosparcm createuser --email "erha6448@colorado.edu" \
+                      --password "12345" \
+                      --username "erha6448" \
+                      --firstname "Erik" \
+                      --lastname "Hartwick"
